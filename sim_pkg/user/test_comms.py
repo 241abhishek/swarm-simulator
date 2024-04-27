936dc6738d3a63073e2f4312e89f973287b377f5
Abhishek <241abhishek@gmail.com>
2024-04-27 12:50:28 -0500

    Implemented flocking logic

--- sim_pkg/user/test_comms.py
+++ sim_pkg/user/test_comms.py
@@ -4,9 +4,12 @@ import math
 import random
 
 d = 0.5 # repulsion distance threshold
-r = 10.0 # repulsion constant
-a = 20.0 # attraction constant
-s = 1.0 # steering constant
+r = 1.5 # repulsion constant
+a = 0.8 # attraction constant
+k = 2.0 # heading constant
+m = 20.0 # migration constant
+s = 0.5 # steering constant
+n = 5 # number of neighbors
 
 def message(robot):
     if robot.id == 0:
@@ -20,8 +23,7 @@ def message(robot):
 
 def center(robot):
     """
-    Function to congregagte all robots to the
-    center of the arena.
+    Function to implement the flocking algorithm.
 
     Args:
         robot (_type_): robot instance
@@ -41,19 +43,45 @@ def center(robot):
         # calculate the vector to the center
         vec_center = np.array([0.0-pose_t[0], 0.0-pose_t[1]])
         vec_center = vec_center/np.linalg.norm(vec_center) # normalize the vector
-        vec_desired = np.add(vec_desired, a*vec_center) # add the vector to the desired vector
+        vec_desired = np.add(vec_desired, m*vec_center) # add the vector to the desired vector
         vec_desired = vec_desired/np.linalg.norm(vec_desired) # normalize the vector
 
         msgs = robot.recv_msg()
         if len(msgs) > 0:
-            # find calculate the distance from the closest neighbor
+            # calculate the average heading of all neighbors
+            avg_heading = 0.0
+            for i in range(len(msgs)):
+                avg_heading += msgs[i][2]
+            avg_heading = avg_heading/len(msgs)
+            vec_heading = np.array([np.cos(avg_heading), np.sin(avg_heading)]) # conver to vector form
+            vec_heading = vec_heading/np.linalg.norm(vec_heading) # normalize the vector
+            vec_desired = np.add(vec_desired, k*vec_heading) # add the vector to the desired vector
+            vec_desired = vec_desired/np.linalg.norm(vec_desired) # normalize the vector
+
+            # find the COM of n closest neighbors
+            com = np.array([0.0, 0.0])
+            closest_neighbors = []
             min_dist = 1e10
             closest = 0
             for i in range(len(msgs)):
                 dist = math.dist([pose_t[0], pose_t[1]], [msgs[i][0], msgs[i][1]])
+                closest_neighbors.append([dist, i])
                 if dist < min_dist:
                     min_dist = dist
                     closest = i
+
+            # sort the neighbors based on distance
+            closest_neighbors.sort()
+            num_neighbors = min(n, len(closest_neighbors))
+            for i in range(num_neighbors):
+                com = np.add(com, [msgs[closest_neighbors[i][1]][0], msgs[closest_neighbors[i][1]][1]])
+            com = com/n
+
+            # calculate the attraction vector
+            vec_att = np.array([com[0]-pose_t[0], com[1]-pose_t[1]])
+            vec_att = vec_att/np.linalg.norm(vec_att)   # normalize the vector
+            vec_desired = np.add(vec_desired, a*vec_att)
+            vec_desired = vec_desired/np.linalg.norm(vec_desired) # normalize the vector
 
             # if the distance is less than 1.0, calculate and add the repulsion vector
             if min_dist < d:
