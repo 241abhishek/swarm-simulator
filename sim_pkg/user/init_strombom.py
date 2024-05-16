5ef58c080472fc952e46ec85602bbf80012362cf
Abhishek <241abhishek@gmail.com>
2024-05-16 11:29:16 -0500

    Modified the code to give each sheep a unique virtual id

--- sim_pkg/user/init_strombom.py
+++ sim_pkg/user/init_strombom.py
@@ -17,13 +17,13 @@ def init(swarmsize,x,y,theta,a_ids):
         x[0] = random.uniform(0.0, (arena_width/2.0))
         y[0] = random.uniform(-(arena_length/2.0), 0.0)
     theta[0] = random.uniform(-math.pi, math.pi)
-    a_ids[0] = 1 # id for shepherd
+    a_ids[0] = 0 # id for shepherd
 
     # initialze the position of the shepherd in one of the other three quadrants
     for i in range(1, swarmsize):
         x[i] = random.uniform(0.0, (arena_width/2.0))
         y[i] = random.uniform(0.0, (arena_length/2.0))
         theta[i] = random.uniform(-math.pi, math.pi)
-        a_ids[i] = 0 # id for sheep
+        a_ids[i] = i # id for sheep
 
     return x, y, theta, a_ids 
--- sim_pkg/user/strombom.py
+++ sim_pkg/user/strombom.py
@@ -42,6 +42,9 @@ wheel_radius = 0.015
 wheel_distance = 0.08
 point_dist = 0.08 # distance between the point whoose velocity is to be calculated and the robot center
 
+agg_sheep_msgs = [] # aggregated sheep messages
+timestep_count = 0
+
 # create a function to parse a txt file and assign the values to the user variables
 def read_from_txt(filepath):
     global N, n, r_s, r_a, p_a, c, p_s, h, e, p, f_N, sheep_speed, shepherd_speed, st_con, sts_con, goal_x, goal_y
@@ -127,6 +130,7 @@ def shepherd(robot):
     Args:
         robot (robot_instance): The robot object to control.
     """
+    global agg_sheep_msgs, timestep_count
     # read the user variables from the txt file
     read_from_txt("user/strombom_variables.txt")
     # print(f" User Variables: N={N}, n={n}, r_s={r_s}, r_a={r_a}, p_a={p_a}, c={c}, p_s={p_s}, h={h}, e={e}, p={p}, f_N={f_N}, sheep_speed={sheep_speed}, st_con={st_con}, shepherd_speed={shepherd_speed}, sts_con={sts_con}, goal_x={goal_x}, goal_y={goal_y}")
@@ -134,7 +138,7 @@ def shepherd(robot):
     # empty desired vector initialization
     vec_desired = np.array([0.0, 0.0])
 
-    goal_pos = np.array([goal_x, goal_y]) # goal position for the shepherd
+    goal_pos = np.array([goal_x, goal_y]) # goal position
 
     # get robot pose
     pose_t = robot.get_pose()
@@ -146,9 +150,24 @@ def shepherd(robot):
 
         msgs = robot.recv_msg() # receive messages from other robots
         if len(msgs) > 0:
+            print(f"Received messages: {msgs}")
             # check if any other sheep are within the repulsion distance
             # first isolate the sheep messages using the virtual id in the message
-            sheep_msgs = [msg for msg in msgs if msg[3] == 0]
+            sheep_msgs = [msg for msg in msgs if msg[3] != 0]
+            # aggregate the sheep messages
+            for msg in sheep_msgs:
+                agg_sheep_msgs.append(msg)
+            timestep_count += 1
+
+            # only proceed with the rest of the code every 10 time steps
+            if timestep_count == 10:
+                # reset the timestep count
+                timestep_count = 0
+                sheep_msgs = agg_sheep_msgs
+                agg_sheep_msgs = []
+            else:
+                return
+
             # sort the sheep messages based on distance
             closest_neighbors = []
             for i in range(len(sheep_msgs)):
@@ -166,6 +185,9 @@ def shepherd(robot):
                     gcm = np.add(gcm, np.array([sheep_msgs[i][0], sheep_msgs[i][1]]))
                 gcm = gcm/len(sheep_msgs)
 
+                # write the gcm point to a txt file
+                write_points_to_txt("user/shepherd_goal.txt", gcm)
+
                 # check if all the sheep are within the f_N distance from the GCM
                 all_within_f_N = True
                 for i in range(len(sheep_msgs)):
@@ -176,7 +198,7 @@ def shepherd(robot):
 
                 if all_within_f_N:
                     # driving mode
-                    print("Driving mode")
+                    # print("Driving mode")
                     # calculate the equation of the line connecting the GCM and the goal position
                     if (gcm[0] - goal_pos[0]) == 0:
                         theta = math.pi/2
@@ -192,11 +214,11 @@ def shepherd(robot):
                     dist_2 = np.linalg.norm(goal_point_2 - goal_pos)
                     # set goal point to be the point that is farther from the goal position
                     goal_point = goal_point_1 if dist_1 > dist_2 else goal_point_2
-                    print(f"Current Position: {pose_t}")
-                    print(f"Goal Point: {goal_point}")
+                    # print(f"Current Position: {pose_t}")
+                    # print(f"Goal Point: {goal_point}")
 
                     # write the goal point to a txt file
-                    write_points_to_txt("user/shepherd_goal.txt", goal_point)
+                    # write_points_to_txt("user/shepherd_goal.txt", goal_point)
 
                     # calculate the vector to the goal point
                     vec_goal = np.array([goal_point[0]-pose_t[0], goal_point[1]-pose_t[1]])
@@ -206,7 +228,7 @@ def shepherd(robot):
                     vec_desired = vec_desired/np.linalg.norm(vec_desired) # normalize the vector
                 else:
                     # collecting mode
-                    print("Collecting mode")
+                    # print("Collecting mode")
                     # calculate the position of the sheep farthest from the GCM
                     farthest_sheep = np.array([0.0, 0.0])
                     max_dist = 0.0
@@ -231,9 +253,11 @@ def shepherd(robot):
                     dist_2 = np.linalg.norm(goal_point_2 - gcm)
                     # set goal point to be the point that is farther from the gcm
                     goal_point = goal_point_1 if dist_1 > dist_2 else goal_point_2
+                    # print(f"Current Position: {pose_t}")
+                    # print(f"Goal Point: {goal_point}")
 
                     # write the goal point to a txt file
-                    write_points_to_txt("user/shepherd_goal.txt", goal_point)
+                    # write_points_to_txt("user/shepherd_goal.txt", goal_point)
 
                     # calculate the vector to the goal point
                     vec_goal = np.array([goal_point[0]-pose_t[0], goal_point[1]-pose_t[1]])
@@ -242,24 +266,24 @@ def shepherd(robot):
                     vec_desired = np.add(vec_desired, vec_goal)
                     vec_desired = vec_desired/np.linalg.norm(vec_desired) # normalize the vector
 
-                # # calculate the error in heading wrt the desired movement direction
-                # heading_error = math.atan2(np.linalg.det([vec_curr, vec_desired]), np.dot(vec_desired, vec_curr))
+                # calculate the error in heading wrt the desired movement direction
+                heading_error = math.atan2(np.linalg.det([vec_curr, vec_desired]), np.dot(vec_desired, vec_curr))
 
-                # if heading_error > 0:
-                #     robot.set_vel(shepherd_speed, shepherd_speed + shepherd_speed*sts_con*(abs(heading_error)/np.pi))
-                # else:
-                #     robot.set_vel(shepherd_speed + shepherd_speed*sts_con*(abs(heading_error)/np.pi), shepherd_speed)
+                if heading_error > 0:
+                    robot.set_vel(shepherd_speed, shepherd_speed + shepherd_speed*sts_con*(abs(heading_error)/np.pi))
+                else:
+                    robot.set_vel(shepherd_speed + shepherd_speed*sts_con*(abs(heading_error)/np.pi), shepherd_speed)
                     
-                # calculate the wheel velocities using the differential drive motion model
-                wheel_velocities = diff_drive_motion_model(vec_desired, pose_t)
+                # # calculate the wheel velocities using the differential drive motion model
+                # wheel_velocities = diff_drive_motion_model(vec_desired, pose_t)
 
-                # normalize and scale the wheel velocities
-                max_wheel_vel = max(abs(wheel_velocities[0]), abs(wheel_velocities[1]))
-                wheel_velocities = wheel_velocities/max_wheel_vel
-                wheel_velocities = wheel_velocities*shepherd_speed
+                # # normalize and scale the wheel velocities
+                # max_wheel_vel = max(abs(wheel_velocities[0]), abs(wheel_velocities[1]))
+                # wheel_velocities = wheel_velocities/max_wheel_vel
+                # wheel_velocities = wheel_velocities*shepherd_speed
 
-                # set the wheel velocities
-                robot.set_vel(wheel_velocities[0], wheel_velocities[1])
+                # # set the wheel velocities
+                # robot.set_vel(wheel_velocities[0], wheel_velocities[1])
 
         # construct message to send to other robots
         pose_t.append(float(robot.virtual_id))
@@ -306,35 +330,35 @@ def sheep(robot):
             vec_desired = np.add(vec_desired, arena_rep*vec_boundary)
             vec_desired = vec_desired/np.linalg.norm(vec_desired) # normalize the vector
 
-            # # move only if the sheep is prompted to move
-            # if vec_desired[0] != 0.0 or vec_desired[1] != 0.0:
-            #     # calculate the error in heading wrt the desired movement direction
-            #     heading_error = math.atan2(np.linalg.det([vec_curr, vec_desired]), np.dot(vec_desired, vec_curr))
-
-            #     if heading_error > 0:
-            #         robot.set_vel(sheep_speed, sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi)) # turn left
-            #     else:
-            #         robot.set_vel(sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi), sheep_speed) # turn right
-            # else:
-            #     # print(f"Stopping sheep {robot.virtual_id} near the arena boundary")
-            #     robot.set_vel(0.0, 0.0) # stop moving
-
             # move only if the sheep is prompted to move
             if vec_desired[0] != 0.0 or vec_desired[1] != 0.0:
-                # use the differential drive motion model to calculate the wheel velocities
-                wheel_velocities = diff_drive_motion_model(vec_desired, pose_t)
+                # calculate the error in heading wrt the desired movement direction
+                heading_error = math.atan2(np.linalg.det([vec_curr, vec_desired]), np.dot(vec_desired, vec_curr))
 
-                # normalize and scale the wheel velocities
-                max_wheel_vel = max(abs(wheel_velocities[0]), abs(wheel_velocities[1]))
-                wheel_velocities = wheel_velocities/max_wheel_vel
-                wheel_velocities = wheel_velocities*sheep_speed
-
-                # set the wheel velocities
-                robot.set_vel(wheel_velocities[0], wheel_velocities[1])
-
+                if heading_error > 0:
+                    robot.set_vel(sheep_speed, sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi)) # turn left
+                else:
+                    robot.set_vel(sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi), sheep_speed) # turn right
             else:
                 # print(f"Stopping sheep {robot.virtual_id} near the arena boundary")
                 robot.set_vel(0.0, 0.0) # stop moving
+
+            # # move only if the sheep is prompted to move
+            # if vec_desired[0] != 0.0 or vec_desired[1] != 0.0:
+            #     # use the differential drive motion model to calculate the wheel velocities
+            #     wheel_velocities = diff_drive_motion_model(vec_desired, pose_t)
+
+            #     # normalize and scale the wheel velocities
+            #     max_wheel_vel = max(abs(wheel_velocities[0]), abs(wheel_velocities[1]))
+            #     wheel_velocities = wheel_velocities/max_wheel_vel
+            #     wheel_velocities = wheel_velocities*sheep_speed
+
+            #     # set the wheel velocities
+            #     robot.set_vel(wheel_velocities[0], wheel_velocities[1])
+
+            # else:
+            #     # print(f"Stopping sheep {robot.virtual_id} near the arena boundary")
+            #     robot.set_vel(0.0, 0.0) # stop moving
             
             # construct message to send to other robots
             pose_t.append(float(robot.virtual_id))
@@ -347,7 +371,7 @@ def sheep(robot):
         if len(msgs) > 0:
             # check if any other sheep are within the repulsion distance
             # first isolate the sheep messages using the virtual id in the message
-            sheep_msgs = [msg for msg in msgs if msg[3] == 0]
+            sheep_msgs = [msg for msg in msgs if msg[3] != 0]
             # sort the sheep messages based on distance
             closest_neighbors = []
             for i in range(len(sheep_msgs)):
@@ -375,7 +399,7 @@ def sheep(robot):
                 
             # calculate the distance to the shepherd
             # first isolate the shepherd message using the virtual id in the message
-            shepherd_msg = [msg for msg in msgs if msg[3] == 1]
+            shepherd_msg = [msg for msg in msgs if msg[3] == 0]
             shepherd_msg = shepherd_msg[0] if len(shepherd_msg) > 0 else None # pick the first message if multiple messages are received
             if shepherd_msg is not None:
                 # calculate the distance to the shepherd
@@ -404,32 +428,32 @@ def sheep(robot):
                     vec_desired = np.add(vec_desired, c*vec_attraction)
                     vec_desired = vec_desired/np.linalg.norm(vec_desired)
 
-        # # move only if the sheep is prompted to move
-        # if vec_desired[0] != 0.0 or vec_desired[1] != 0.0:
-        #     # calculate the error in heading wrt the desired movement direction
-        #     heading_error = math.atan2(np.linalg.det([vec_curr, vec_desired]), np.dot(vec_desired, vec_curr))
-
-        #     if heading_error > 0:
-        #         robot.set_vel(sheep_speed, sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi)) # turn left
-        #     else:
-        #         robot.set_vel(sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi), sheep_speed) # turn right
-        # else:
-        #     robot.set_vel(0.0, 0.0) # stop moving
-                    
         # move only if the sheep is prompted to move
         if vec_desired[0] != 0.0 or vec_desired[1] != 0.0:
-            # use the differential drive motion model to calculate the wheel velocities
-            wheel_velocities = diff_drive_motion_model(vec_desired, pose_t)
+            # calculate the error in heading wrt the desired movement direction
+            heading_error = math.atan2(np.linalg.det([vec_curr, vec_desired]), np.dot(vec_desired, vec_curr))
 
-            # normalize and scale the wheel velocities
-            max_wheel_vel = max(abs(wheel_velocities[0]), abs(wheel_velocities[1]))
-            wheel_velocities = wheel_velocities/max_wheel_vel
-            wheel_velocities = wheel_velocities*sheep_speed
-
-            # set the wheel velocities
-            robot.set_vel(wheel_velocities[0], wheel_velocities[1])
+            if heading_error > 0:
+                robot.set_vel(sheep_speed, sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi)) # turn left
+            else:
+                robot.set_vel(sheep_speed + sheep_speed*st_con*(abs(heading_error)/np.pi), sheep_speed) # turn right
         else:
             robot.set_vel(0.0, 0.0) # stop moving
+                    
+        # # move only if the sheep is prompted to move
+        # if vec_desired[0] != 0.0 or vec_desired[1] != 0.0:
+        #     # use the differential drive motion model to calculate the wheel velocities
+        #     wheel_velocities = diff_drive_motion_model(vec_desired, pose_t)
+
+        #     # normalize and scale the wheel velocities
+        #     max_wheel_vel = max(abs(wheel_velocities[0]), abs(wheel_velocities[1]))
+        #     wheel_velocities = wheel_velocities/max_wheel_vel
+        #     wheel_velocities = wheel_velocities*sheep_speed
+
+        #     # set the wheel velocities
+        #     robot.set_vel(wheel_velocities[0], wheel_velocities[1])
+        # else:
+        #     robot.set_vel(0.0, 0.0) # stop moving
         
         # construct message to send to other robots
         pose_t.append(float(robot.virtual_id))
@@ -437,7 +461,7 @@ def sheep(robot):
         robot.send_msg(msg)    
 
 def usr(robot):
-    if robot.id == 0:
+    if robot.virtual_id == 0:
         robot.set_led(0, 0, 255)
         while True:
             shepherd(robot)
